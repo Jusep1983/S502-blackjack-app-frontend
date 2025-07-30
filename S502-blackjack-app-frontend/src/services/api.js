7793e9e2029e88b0122138335b8174d8b57709cd
Jusep1983 <j.lopezcalvo1983@gmail.com>
2025-07-30 15:53:39 +0200

    fix: update api baseURL for Render

--- S502-blackjack-app-frontend/src/services/api.js
+++ S502-blackjack-app-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080", // he cambiado esto
 });
 
 export default api;
